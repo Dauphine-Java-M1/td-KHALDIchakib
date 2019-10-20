ac2dbc6ecd1cb77fefd48fd976ad3e5db087f700
KHALDIchakib <33155166+KHALDIchakib@users.noreply.github.com>
2019-10-21 00:07:46 +0200

    Delete TestPoint.java

--- TestPoint.java
+++
@@ -1,9 +0,0 @@
-package fr.dauphine.ja.khaldichakib.td01;
-
-public class TestPoint {
-
-	public static void main(String[] args) {
-		Point p=new Point(0,0);
-		System.out.println(p.getX()+" "+p.getY());
-	}
-}
